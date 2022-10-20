01af352b78e10e6ef3e07ec3be62e4127e0dba2f
t-katsumura <t-katsumura@nri.co.jp>
2022-10-21 03:44:51 +0900

    remove unused code

--- wrapper.go
+++ wrapper.go
@@ -20,19 +20,6 @@ func (h *HandlerFuncWrapper) PreMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func GetPreMiddleware(handlerFunc http.HandlerFunc) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if handlerFunc != nil {
-				handlerFunc(w, r)
-			}
-			if next != nil {
-				next.ServeHTTP(w, r)
-			}
-		})
-	}
-}
-
 // Wrap http handler function as http handler.
 // Wrapped function is executed after invoking proceeding handlers.
 // This is what `Post` means.
@@ -45,19 +32,6 @@ func (h *HandlerFuncWrapper) PostMiddleware(next http.Handler) http.Handler {
 			h.HandlerFunc(w, r)
 		}
 	})
-}
-
-func GetPostMiddleware(handlerFunc http.HandlerFunc) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if next != nil {
-				next.ServeHTTP(w, r)
-			}
-			if handlerFunc != nil {
-				handlerFunc(w, r)
-			}
-		})
-	}
 }
 
 // Alias for `PreMiddleware`
